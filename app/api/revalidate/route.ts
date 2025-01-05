b7cb4d284a91b73f782863671868b588839ee29b
Daniel Trochez <daniel@monkeysolutions.se>
2025-01-05 23:48:47 +0100

    removed debugging logs

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,4 @@
-import {
-  getSanityDataFromCache,
-  revalidateCache,
-} from "@/lib/api/sanityDataLoader";
+import { revalidateCache } from "@/lib/api/sanityDataLoader";
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -12,28 +9,9 @@ export async function POST(request: NextRequest) {
   }
 
   try {
-    const dataBeforeRevalidated = await getSanityDataFromCache();
-    const projectsDataBeforeRevalidation = dataBeforeRevalidated?.find(
-      (data) => data._type === "project" && data.client === "Monkey Solutions"
-    );
-    console.log(
-      "Title BEFORE revalidation: ",
-      projectsDataBeforeRevalidation?.title
-    );
-
     revalidatePath("/api/sanity-data");
     revalidatePath("/");
     await revalidateCache();
-    // revalidatePath("/api/sanity-data");
-
-    const dataAfterRevalidated = await getSanityDataFromCache();
-    const projectsDataAfterRevalidation = dataAfterRevalidated?.find(
-      (data) => data._type === "project" && data.client === "Monkey Solutions"
-    );
-    console.log(
-      "Title AFTER revalidation: ",
-      projectsDataAfterRevalidation?.title
-    );
 
     const response = NextResponse.json("Successful reloading of sanity data!", {
       status: 200,
